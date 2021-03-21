63227598beff612a19026163b9495c3a7ca60d86
Lucas Santos <leos9877@gmail.com>
2021-03-21 19:43:25 -0300

    implementando alterações  - foi adicionando arquivo java-script  - foi implementada função para cadastrar dados dinâmicas em listas  - foi modificado o form para realizar o cadastro dinâmico  - foi modificado a estrutura base do html de acordo com a atividade

---
+++ static/js/index.js
@@ -0,0 +1,9 @@
+function cadastrar() {
+    var linha = document.createElement('tr');
+    linha.insertCell(0).innerHTML += document.getElementById('nome').value;
+    linha.insertCell(1).innerHTML += document.getElementById('email').value; 
+    linha.insertCell(2).innerHTML += document.getElementById('telefone').value;
+    document.getElementById('tabela-pessoa').appendChild(linha);
+
+    return false;
+}
