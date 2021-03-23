486bec9ae3a7019976f3ca8ad4d129e61705f814
Lucas Santos <leos9877@gmail.com>
2021-03-22 22:41:33 -0300

    implementando alterações

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,8 @@
-function cadastrar() {
+function cadastrar(form) {
     var linha = document.createElement('tr');
-    linha.insertCell(0).innerHTML += document.getElementById('nome').value;
-    linha.insertCell(1).innerHTML += document.getElementById('email').value; 
-    linha.insertCell(2).innerHTML += document.getElementById('telefone').value;
+    linha.insertCell(0).innerHTML += form.nome.value;
+    linha.insertCell(1).innerHTML += form.email.value; 
+    linha.insertCell(2).innerHTML += form.telefone.value;
     document.getElementById('tabela-pessoa').appendChild(linha);
 
     document.getElementById("nome").value="";
